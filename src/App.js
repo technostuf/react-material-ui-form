e14c087265a459ece1271ce60817914f99fea963
Amit Patel <89689746+technostuf@users.noreply.github.com>
2022-05-01 18:03:31 +0530

    react material form example

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MaterialFormComponent from './MaterialFormComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <MaterialFormComponent />
+    </div>
+  );
+}
+
+export default App;
